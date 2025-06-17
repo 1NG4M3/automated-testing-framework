266abc77f4790890577b72b1d24d86a587b52d28
Dmitry Gusev <dimagyss@mail.ru>
2025-06-17 18:38:16 +0300

    My last changes

--- src/test/java/gusev/ui/UploadAndDownloadPage.java
+++ src/test/java/gusev/ui/UploadAndDownloadPage.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 import static com.codeborne.selenide.Selenide.$x;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -17,7 +18,7 @@ public class UploadAndDownloadPage {
     private final SelenideElement uploadedFilePath = $x("//*[@id='uploadedFilePath']");
 
     @Step("Скачать файл и проверить: текст '{expectedText}', ссылка содержит '{expectedHref}'")
-    public UploadAndDownloadPage assertDownloadFile(String expectedText, String expectedHref) {
+    public UploadAndDownloadPage assertDownloadFile(String expectedText, String expectedHref) throws FileNotFoundException {
         File downloadedFile = downloadButton
                 .shouldBe(Condition.visible)
                 .shouldBe(Condition.enabled)
